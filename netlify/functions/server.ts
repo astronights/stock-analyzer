310da1b798dbc3fdac9cca4ed5841470a156f18e
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-07-09 23:54:36 +0800

    Minor changes

---
+++ netlify/functions/server.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import * as CONFIG from "../../src/config";
+import { startUp, daily, dailyEx, quote, quoteEx } from "../../src/scraper/script";
+import cron from 'node-cron';
+import router from "../../src/api/router";
+import serverless from 'serverless-http';
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use("/", router);
+
+const HOST = CONFIG.HOST;
+const PORT = parseInt(CONFIG.PORT);
+
+const assets = startUp();
+export const quoteArgs = quote.bind(null, assets);
+
+cron.schedule(dailyEx, daily, {
+  timezone: 'Etc/UTC'
+});
+
+cron.schedule(quoteEx, quoteArgs, {
+  timezone: 'Etc/UTC'
+});
+
+app.listen(PORT, () => {
+  console.log(`Started server at ${HOST}:${PORT}`);
+});
+
+module.exports.handler = serverless(app);
