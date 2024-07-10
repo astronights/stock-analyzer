f302a4f202ed4795ad40fa616f38c800700e34bb
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-07-10 09:29:42 +0800

    Small removals

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config({ path: resolve(__dirname, "../.env") });
 
 const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || "3000";
 const PERIOD = parseInt(process.env.PERIOD || "60");
 
 export { HOST, PORT, PERIOD };
