dea490cc6fed02ff2b881e5210ce9fdc64a2687f
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-07-10 10:07:16 +0800

    Reverting to Node

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import * as CONFIG from "../src/config";
-import { startUp, daily, dailyEx, quote, quoteEx } from "../src/scraper/script";
+import * as CONFIG from "./config";
+import { startUp, daily, dailyEx, quote, quoteEx } from "./scraper/script";
 import cron from 'node-cron';
 import bodyParser from 'body-parser';
-import router from "../src/api/router";
+import router from "./api/router";
+import { createServer } from "http";
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,8 +26,6 @@ cron.schedule(quoteEx, quoteArgs, {
     timezone: 'Etc/UTC'
 });
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on ${HOST}:${PORT}`)
-// })
-
-module.exports = app;
+const httpServer = createServer(app).listen(PORT, () => {
+    console.log(`Server running on ${HOST}:${PORT}`)
+})
